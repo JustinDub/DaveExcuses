f9d68c358b5accf359363651cd617ce4594ad0a3
JustinDub <justin.duban@hotmail.com>
2022-02-13 15:34:37 +0100

    fixed server routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -13,7 +13,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/api/list', function(req, res, next) {
-  res.status(200).json({data: excusesList})
+  return res.status(200).json({data: excusesList})
 });
+
+router.post('/api/new', function(req, res, next) {
+  if( excusesList.findIndex((value) => value == req.body.newExcuse) == -1) {
+    excusesList.push(req.body.newExcuse)
+  }
+  return res.status(200).json({newElement: excusesList.lastItem})
+})
 
 module.exports = router;
